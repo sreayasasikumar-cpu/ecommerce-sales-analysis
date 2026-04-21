f91060685fe53acdcf896d1989321d7b3e91b61d
sreayasasikumar-cpu <sreayasasikumar@gmail.com>
2026-04-22 01:59:37 +0530

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,1 +1,23 @@
+-- Total Sales
+SELECT SUM(Sales) AS Total_Sales FROM superstore_cleaned;
 
+-- Sales by Category
+SELECT Category, SUM(Sales) AS Total_Sales
+FROM superstore_cleaned
+GROUP BY Category;
+
+-- Profit by Category
+SELECT Category, SUM(Profit) AS Total_Profit
+FROM superstore_cleaned
+GROUP BY Category;
+
+-- Sales by Region
+SELECT Region, SUM(Sales) AS Total_Sales
+FROM superstore_cleaned
+GROUP BY Region;
+
+-- Yearly Sales Trend
+SELECT YEAR(Order_Date) AS Year, SUM(Sales) AS Total_Sales
+FROM superstore_cleaned
+GROUP BY YEAR(Order_Date)
+ORDER BY Year;
